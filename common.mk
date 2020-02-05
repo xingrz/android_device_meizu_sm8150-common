dbb6395f186b416befe1f5352a1823d19115724b
XiNGRZ <chenxingyu92@gmail.com>
2020-02-05 21:42:50 +0800

    sm8150: Use prebuilt audio HAL
    Change-Id: Ifa12400be64b7fa5cf139cd14bf88ad866c70c9d

--- common.mk
+++ common.mk
@@ -69,14 +69,12 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@4.0-impl \
     android.hardware.soundtrigger@2.1-impl \
-    audio.primary.msmnile \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
     libaudio-resampler \
     libdynproc \
     libtinyalsa \
-    libtinycompress \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
